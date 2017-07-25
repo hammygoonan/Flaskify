af49ff7068b6ec9950494a1cbceb8585a5f07a6d
Hammy Goonan <hammy@falconry.io>
2017-07-25 11:49:34 +1000

    skeleton bits for mp3

--- project/data/mp3.py
+++ project/data/mp3.py
@@ -2,4 +2,58 @@ from .track import Track
 
 
 class Mp3(Track):
-    pass
+    """Extract"""
+    def get_title(self):
+        # TIT2
+        pass
+
+    def get_artists(self):
+        # TPE1
+        pass
+
+    def get_album_artists(self):
+        # TPE2
+        pass
+
+    # def get_album_title(self):
+    #     pass
+
+    def get_album_year(self):
+        # TDRC
+        pass
+
+    # def get_url_for(self):
+    #     pass
+
+    def get_album_pic(self):
+        # APIC
+        pass
+
+    def get_track_album(self):
+        # TALB
+        pass
+
+    # def get_album_name(self):
+    #     pass
+
+    def get_track_style(self):
+        # TCON
+        pass
+
+    def get_track_copyright(self):
+        # TCOP
+        pass
+
+    # def get_track_release_date(self):
+    #     pass
+
+    # def get_track_encoder(self):
+    #     pass
+
+    def get_track_publisher(self):
+        # TPUB
+        pass
+
+    def get_track_number(self):
+        # TRCK
+        pass
